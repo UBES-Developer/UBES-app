2043d1ce3f2e46caccdfeab11cf1e6fd20b4760b
Tirelo Junior Tshukudu <teejaytshukudu@MacBookPro.fritz.box>
2026-01-04 21:35:00 +0200

    Fix: Hide desktop sidebar on mobile devices

--- components/layout/LayoutWrapper.tsx
+++ components/layout/LayoutWrapper.tsx
@@ -20,7 +20,7 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
 
     return (
         <div className="flex h-full bg-gray-50">
-            <Sidebar className="flex" />
+            <Sidebar className="hidden md:flex" />
             <div className="flex flex-1 flex-col overflow-hidden">
                 <Header />
                 <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-20 md:pb-6">
